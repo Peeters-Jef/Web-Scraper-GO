ac813ff990646da1d193f84a505abbb8c8b91ce5
Peeters-Jef <peeters.jef01@gmail.com>
2024-06-25 00:44:53 +0000

    fixed issue with urls ending with a / not changing to lowercase

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,28 +1,28 @@
 package scraper
 
 import (
-	//"net/http"
 	"net/url"
 	"strings"
 )
 
 func NormalizeURL(rawUrl string) string {
-    if !strings.Contains(rawUrl, "://") {
-        rawUrl = "https://" + rawUrl
-    }
-    
     parsedURL, err := url.Parse(rawUrl)
     if err != nil {
         return "no url provided"
     }
 
-    if parsedURL.Scheme != "https" {
-        parsedURL.Scheme = "https"
+    // remove the Scheme
+    normalizedURL := parsedURL.Host + parsedURL.Path
+
+    normalizedURL = strings.ToLower(normalizedURL)
+
+    if strings.HasPrefix(normalizedURL, "//") {
+        return normalizedURL[2:]
+    }
+    
+    if strings.HasSuffix(normalizedURL, "/") {
+        return normalizedURL[:len(normalizedURL)-1]
     }
 
-    parsedURL.Host = parsedURL.Hostname()
-    parsedURL.Path = "/"
-
-    return parsedURL.String()
+    return normalizedURL
 }
-
