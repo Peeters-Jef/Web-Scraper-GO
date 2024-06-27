0caf70c188999437764f8ab0cef6299b385ac5d9
Peeters-Jef <peeters.jef01@gmail.com>
2024-06-27 15:32:17 +0200

    added beginning of crawl function

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,6 +1,8 @@
 package scraper
 
 import (
+	"fmt"
+	"net/http"
 	"net/url"
 	"strings"
 )
@@ -25,4 +27,19 @@ func NormalizeURL(rawUrl string) string {
     }
 
     return normalizedURL
+}
+
+func crawl(url string, visited map[string]bool) {
+    if visited[url] {
+        return
+    }
+
+    visited[url] = true
+    fmt.Println("Visiting:", url)
+
+    resp, err := http.Get(url)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
 }
